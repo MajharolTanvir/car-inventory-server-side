cfdf5e939fbac63a147a2ea098027ba52e5ce313
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-07 11:10:13 +0600

    add item with user

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ async function run() {
     try {
         await client.connect();
         const carCollection = client.db('car-inventory').collection('car-data')
+        const newCollection = client.db('car-inventory').collection('new-car-data')
         
         // get all data
-        // http://localhost:5000/inventorys
         // https://car-inventory-bd.herokuapp.com/inventorys
         app.get('/inventorys', async (req, res) => {
             const query = req.query;
@@ -27,6 +27,15 @@ async function run() {
             const cars = await cursor.toArray()
             res.send(cars)
         })
+
+        // get myProducts
+        app.get('/myProducts?email=', async (req, res) => {
+            const query = req.query;
+            const cursor = carCollection.find(query)
+            const cars = await cursor.toArray()
+            res.send(cars)
+        })
+
 
         // get single data
         app.get('/inventory/:id', async (req, res) => {
@@ -42,9 +51,17 @@ async function run() {
         app.post('/inventorys', async (req, res) => {
             const newCar = req.body;
             console.log(newCar);
-            const car = await carCollection.insertOne(newCar)
+            const car = await newCollection.insertOne(newCar)
             res.send(car);
         })
+        // post new data with user
+        // https://car-inventory-bd.herokuapp.com/inventory
+        // app.post('/inventorys', async (req, res) => {
+        //     const newCar = req.body;
+        //     console.log(newCar);
+        //     const car = await newCollection.insertOne(newCar)
+        //     res.send(car);
+        // })
 
         // Put?Update your data .
         // https://car-inventory-bd.herokuapp.com/inventory/id=${id}
